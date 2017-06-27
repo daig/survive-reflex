bc9360f5662ab7b0d88553fa366404370347a73f
傣जी <dailectic@gmail.com>
2017-06-26 20:34:20 -0700

    Generating the Ids

--- src/Notes.hs
+++ src/Notes.hs
@@ -4,16 +4,20 @@ import Data.Text (Text)
 import Data.Map (Map)
 import qualified Data.Map as Map
 import Data.Semigroup ((<>))
+import Data.Unique
+import Control.Monad.Trans (MonadIO(..))
 
-type Id = Int
+type Id = Unique
 type Note = Text
 type Notes = Map Id Note
 
-initialNotes :: Notes
-initialNotes = 0 =: "Learn Reflex"
-            <> 1 =: "Do laundry"
+initializeNotes :: IO Notes
+initializeNotes = foldMap newNote ["Learn Reflex", "Do laundry"]
+  where newNote n = ffor newUnique (=: n)
 
 notes :: MonadWidget t m => m ()
-notes = (() <$) $ el "ul" $
-  list (constDyn initialNotes) $ \note ->
-    el "li" $ dynText note
+notes = do
+  initialNotes <- constDyn <$> liftIO initializeNotes
+  (() <$) $ el "ul" $
+    list initialNotes $ \note ->
+      el "li" $ dynText note
