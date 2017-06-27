21d23c5d4b66a4a6fb75ab8dbf6237ac66484181
傣जी <dailectic@gmail.com>
2017-06-27 00:06:12 -0700

    Communicating Delection to _Notes_

--- src/Notes.hs
+++ src/Notes.hs
@@ -1,9 +1,10 @@
 module Notes where
+import Utils
 import Note
 import Reflex.Dom
 import Data.Map (Map)
 import qualified Data.Map as Map
-import Data.Semigroup ((<>))
+import Data.Semigroup ((<>),Endo(..))
 import Data.Unique
 import Control.Monad.Trans (MonadIO(..))
 
@@ -14,9 +15,10 @@ newNote :: Note -> IO Notes
 newNote n = ffor newUnique (=: n)
 
 notes :: MonadWidget t m => IO Notes -> Event t Notes -> m (Dynamic t Notes)
-notes initializeNotes addNote = do
+notes initializeNotes (fmap (Endo . Map.union) -> addNote) = do
   initialNotes <- liftIO initializeNotes
-  notesDyn <- foldDyn Map.union initialNotes addNote
-  _ <- el "ul" $ list notesDyn $ \n ->
-         el "li" $ note n
+  rec notesDyn <- foldDyn appEndo initialNotes (addNote <> deleteNote)
+      deleteNote <- fmap mergeDynFoldableEvents $
+        el "ul" $ listWithKey notesDyn $ \i n ->
+          el "li" $ (Endo (Map.delete i) <$) <$> note n
   return notesDyn
--- src/Utils.hs
+++ src/Utils.hs
@@ -1,12 +1,18 @@
-{-# LANGUAGE FlexibleContexts #-}
 module Utils where
 import Reflex.Dom
 import Control.Monad.Trans (MonadIO(..))
 import Data.Text (Text)
 import qualified Data.Text as T
+import Data.Foldable
+import Data.Semigroup (Semigroup(..))
+import Data.Map (Map)
+import qualified Data.Map as Map
 
 putStrLn' :: MonadIO m => Text -> m ()
 putStrLn' t = liftIO $ putStrLn (T.unpack t)
 
 onEvent :: PerformEvent t m => (a -> Performable m b) -> Event t a -> m (Event t b)
 onEvent m = performEvent . fmap m
+
+mergeDynFoldableEvents :: (Reflex t, Semigroup a, Foldable f) => Dynamic t (f (Event t a)) -> Event t a
+mergeDynFoldableEvents = switchPromptlyDyn . fmap fold
