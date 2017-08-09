0b502b6fc3793d8592628632e87fa1da86da8a13
Dai <dailectic@gmail.com>
2017-08-09 11:27:34 -0700

    Breaking out Note rendering

--- src/App.hs
+++ src/App.hs
@@ -7,15 +7,18 @@ app :: MonadWidget t m => m ()
 {-app = return ()-}
 app = do
   initNotes <- getNotes
+
   addNote <- performEvent . (liftIO newUnique <$) =<< button "+"
   notes <- foldDyn (\k m -> M.insert k "new note" m) initNotes addNote
-  _ <- ul_ $ listWithKey notes $ \_ v ->
-    li_ $ dynText v
+  _ <- ul_ $ listWithKey notes $ (\_ v -> li_ $ renderNote v)
   return ()
 
-  
+renderNote :: MonadWidget t m => Dynamic t Text -> m ()
+renderNote = dynText
 
-type Notes = Map Unique Text
+type Id = Unique
+instance Show Unique where show = show . hashUnique
+type Notes = Map Id Text
 
 getNotes :: MonadIO m => m Notes
 getNotes = liftIO $ foldMap (\x -> newUnique <&> (=: x)) ["Bend","Cheese it"] 
